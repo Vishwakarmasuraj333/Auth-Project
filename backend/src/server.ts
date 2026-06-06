ffca72954c327c63507007974dfdf1a779952885
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2026-06-06 15:35:28 +0530

    Update CORS origins and refactor server routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,18 +12,20 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "http://localhost:3001",
       "https://auth-project-h1vy.vercel.app",
-      "https://auth-project-three-theta.vercel.app",
+      "https://auth-project-h1vy-git-main-vishwakarmasuraj333s-projects.vercel.app",
+      "https://auth-project-h1vy-c7at27is5-vishwakarmasuraj333s-projects.vercel.app",
     ],
     credentials: true,
   })
 );
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend Running ✅");
 });
-
-app.use("/api/auth", authRoutes);
 
 connectDB();
 
