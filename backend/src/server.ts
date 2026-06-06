30887d2ec7f99d3bbfdbf316c77246a7b821ae14
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2026-06-06 15:18:47 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import cors from "cors";
 import app from "./app";
 import { connectDB } from "./config/db";
 import authRoutes from "./routes/auth.routes";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://auth-project-h1vy.vercel.app",
+      "https://auth-project-three-theta.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running ✅");
+});
 
 app.use("/api/auth", authRoutes);
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
